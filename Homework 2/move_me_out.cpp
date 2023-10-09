979b08259beef4174e2005dfc2974c0724986c39
Kotiaev <146478288+Kotiaev@users.noreply.github.com>
2023-10-09 09:43:54 +0000

    corrected

--- Homework 2/move_me_out.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "move_me_out.h"
-
-void example_function();
-void todo_move_me_out() {
-	std::cout << "This function should be located in the separated source file" << std::endl;
-} 
--- Homework 2/move_me_out.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void example_function();
-void todo_move_me_out() ;
